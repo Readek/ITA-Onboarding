26c33670115718ea5f985dc161c98d2c584537d4
Readek <readeku@gmail.com>
2024-10-08 10:44:37 +0200

    Exercici 2 - useState

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
 
   const [ step, setStep ] = useState(0);
 
+  function handleClick() {
+    nextStep();
+  }
+
+  /** Updates to the next step or goes back to the first if at end */
+  function nextStep() {
+    if (step == tutorialData.length - 1) {
+      setStep(0);
+    } else {
+      setStep(step+1);
+    }
+  }
+
   return (
     <div className=''>
       <Card
@@ -14,6 +27,7 @@ function App() {
         desc={tutorialData[step].description}
         bgCol={tutorialData[step].bgColor}
         img={tutorialData[step].image}
+        clickEv={handleClick}
       />
     </div>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
-export function Card({ title, desc, bgCol, img}) {
+export function Card({ title, desc, bgCol, img, clickEv }) {
     
     return(
         <div className="" style={{backgroundColor: bgCol}}>
             <div>{title}</div>
             <div>{desc}</div>
             <img src={img} alt="Card's image" />
+            <button onClick={clickEv}>Next</button>
         </div>
     )
 
